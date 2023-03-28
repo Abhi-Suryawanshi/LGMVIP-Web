5d06a665b13d7787349bc026e39f099f87afa408
Abhishek Suryawanshi <126605338+Abhi-Suryawanshi@users.noreply.github.com>
2023-03-28 20:23:26 +0530

    Add files via upload

---
+++ Registration form/script.js
@@ -0,0 +1,36 @@
+function displayInput() {
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('mail').value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+
+    const studentData = document.createElement('div');
+    studentData.classList.add('student-data');
+
+    const nameElement = document.createElement('p');
+    nameElement.innerHTML = '<b>Name:</b> ' + name;
+    studentData.appendChild(nameElement);
+    
+    const phoneElement = document.createElement('p');
+    phoneElement.innerHTML = '<b>Contact Number:</b> ' + phone;
+    studentData.appendChild(phoneElement);
+    
+    const emailAddress = document.createElement('p');
+    emailAddress.innerHTML = '<b>Email ID</b>:' + email;
+    studentData.appendChild(emailAddress);
+ 
+    const genderElement = document.createElement('p');
+    genderElement.innerHTML = '<b>Gender:<b> ' + (male.checked ? 'Male' : 'Female');
+    studentData.appendChild(genderElement);
+    
+    const studentInfo = document.getElementById('student-info');
+    studentInfo.appendChild(studentData);
+
+    document.getElementById('name').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('mail').value = '';
+    document.getElementById('male').checked = false;
+    document.getElementById('female').checked = false;
+
+    return false;
+}
